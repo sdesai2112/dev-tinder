7f28adfd0938c1efc0ee2fb9dd6a147c4bb000d9
Shraddha <sshradhadesai@gmail.com>
2025-07-24 15:03:48 +0530

    Exploring the routes

--- src/app.js
+++ src/app.js
@@ -2,12 +2,20 @@ const express = require("express");
 
 const app = express();
 
-app.use("/test", (req, res) => {
-  res.send("Hello from Server..!!");
+app.get("/user", (req, res) => {
+  res.send({
+    name: "Shraddha",
+    surname: "Desai",
+  });
 });
 
-app.use("/hello", (req, res) => {
-  res.send("Hello Hello Hello...!!");
+app.post("/user", (req, res) => {
+  console.log("Saving data to DB....");
+  res.send("Saved data to Database..!!");
+});
+
+app.use("/test", (req, res) => {
+  res.send("Hello from Server..!!");
 });
 
 app.listen(3000, () => {
