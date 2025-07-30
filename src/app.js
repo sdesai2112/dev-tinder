ad4b66ef57b8e45497605e5f27dd5191cebaa0e9
Shraddha <sshradhadesai@gmail.com>
2025-07-30 21:55:59 +0530

    Adding data to database by creating mongo schema and model

--- src/app.js
+++ src/app.js
@@ -1,7 +1,25 @@
 const express = require("express");
 const connectDB = require("./config/database");
+const User = require("./models/user");
 
 const app = express();
+
+app.post("/signup", async (req, res) => {
+  console.log("Req: ", req.body);
+  // Creating new instance of User model.s
+  const user = new User({
+    firstName: "Shraddha",
+    lastName: "Desai",
+    emailId: "saee@gmail.com",
+    password: "saee@123",
+    age: 28,
+  });
+
+  const userData = await user.save();
+  console.log("User Created Successfully", userData);
+
+  res.status(201).send("User Created Successfully");
+});
 
 connectDB()
   .then(() => {
---
+++ src/models/user.js
@@ -0,0 +1,24 @@
+const { Schema, default: mongoose } = require("mongoose");
+
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+  emailId: {
+    type: String,
+  },
+  password: {
+    type: String,
+  },
+  age: {
+    type: Number,
+  },
+  gender: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
